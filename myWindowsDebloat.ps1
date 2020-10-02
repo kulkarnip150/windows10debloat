fd28a8816190aaa2eee130d8281db7eae8caad66
kulkarnip150 <39945169+kulkarnip150@users.noreply.github.com>
2020-10-02 15:58:02 +0530

    Update myWindowsDebloat.ps1

--- myWindowsDebloat.ps1
+++ myWindowsDebloat.ps1
@@ -1897,6 +1897,9 @@ Function UninstallMsftBloat {
 	Get-AppxPackage "Microsoft.WindowsSoundRecorder" | Remove-AppxPackage
 	Get-AppxPackage "Microsoft.ZuneMusic" | Remove-AppxPackage
 	Get-AppxPackage "Microsoft.ZuneVideo" | Remove-AppxPackage
+	Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
+	Get-AppxPackage *Microsoft.YourPhone* -AllUsers | Remove-AppxPackage
+	Get-AppxPackage *officehub* | Remove-AppxPackage
 }
 
 # Install default Microsoft applications
